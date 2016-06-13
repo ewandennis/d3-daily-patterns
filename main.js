cbf631b32b6d4b0643b4c3edf77ec0ae40b49ed7
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-06-13 20:23:56 +0100

    Correct for BST

--- main.js
+++ main.js
@@ -5,12 +5,29 @@
  */
 
 // Parse the slightly odd incoming date format
+let bstWindows = {
+  2013: [Date.UTC(2013, 2, 31), Date.UTC(2013, 9, 27)],
+  2014: [Date.UTC(2014, 2, 30), Date.UTC(2014, 9, 26)],
+  2015: [Date.UTC(2015, 2, 29), Date.UTC(2015, 9, 25)],
+  2016: [Date.UTC(2016, 2, 27), Date.UTC(2016, 9, 30)]
+};
+
 function parseTimestamp(timestr) {
   let dateRE = /^\s*(\d+)\/(\d+)\/(\d+),\s+(\d+):(\d+)\s*$/
     , dateParts = dateRE.exec(timestr)
     , hours = +dateParts[4]
-    , mins = +dateParts[5];
-    return new Date(dateParts[3], dateParts[2], dateParts[1], hours, mins);
+    , mins = +dateParts[5]
+    , date = new Date(Date.UTC(dateParts[3], dateParts[2]-1, dateParts[1], hours, mins));
+
+  // Incoming dates are in local time: correct BST -> UTC where appropriate
+  if (bstWindows.hasOwnProperty(date.getFullYear())) {
+    let bst = bstWindows[date.getFullYear()];
+    if (date >= bst[0] && date < bst[1]) {
+      date.setHours(date.getHours() - 1);
+    }
+  }
+
+  return date;
 }
 
 let svg = d3.select('#canvas'); 
@@ -47,7 +64,7 @@ Promise.all([sleepModel.loadP(), eatModel.loadP()])
   chart.renderModel(eatModel.records, '#CC0000');
 
   // Timezone: BST -> GMT
-  chart.annotate(new Date('2015-10-25T02:00'), 'green');
+  chart.annotate(new Date(Date.UTC(2015, 9, 25, 1, 0)), 'green');
 
   let date = sleepModel.records[0].start;
   let glalat = 55.864237;
